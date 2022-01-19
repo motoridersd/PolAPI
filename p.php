ba68af3e3f9d52d0e02b2d9ecf8c34d9fb29c79f
motoridersd <chochua@gmail.com>
2022-01-19 08:56:12 -0800

    Script now makes one call per dealer and processes the data with that single call. It's faster and more efficient. Work by /u/fire50_. Updated that script to output in CSV, and include Dealer name/code in first column to make filtering better

--- p.php
+++ p.php
@@ -24,10 +24,6 @@ $dealerJSON = '{"data":{"getClosestRetailerFunction":{"retailers":[
 {"code":"USLRV0011","name":"Polestar Princeton","zipcode": "08648","city": "Lawrenceville, NJ","distance":"1912276","latitude":39.1084355,"longitude":-77.1586721}
 ],"province":null}}}';
 
-/*
-$dealerJSON = '{"data":{"getClosestRetailerFunction":{"retailers":[
-{"code":"USLTO0013","name":"Polestar Denver"}]}}}';
-*/
 $dealer_array = json_decode($dealerJSON, true);
 
 // new version has years 2021 and 2022 hard coded in order to receive more cars. no filter value only gets 8 cars ...
@@ -35,7 +31,7 @@ $carsJSON = '{"operationName":"LoadResultsQuery","variables":{"market":"us","inc
 $cars_array = json_decode($carsJSON, true);
 
 foreach ($dealer_array['data']['getClosestRetailerFunction']['retailers'] as $r) {
-    echo "** " . $r['name'] . " (" . $r['code'] . ") ***\n";
+    #echo "** " . $r['name'] . " (" . $r['code'] . ") ***\n";
     $cars_array['variables']['dealerCode'] = $r['code'];
     $ch = curl_init($url); 
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -49,63 +45,26 @@ foreach ($dealer_array['data']['getClosestRetailerFunction']['retailers'] as $r)
     $re_array = json_decode($result, true);
     $want = $re_array['data']['filterFunction']['filterResults'];
 
-    $car_postdata = '{"operationName":"getCarDetail","variables":{"pno34":"534EDPP0E13172900RFA000      00000001147001162XPAINTXPLUSS","market":"US","modelYear":"2022","dealerCode":"USMJI0012","debug":false},"query":"query getCarDetail($pno34: String!, $market: String!, $modelYear: String!, $dealerCode: String, $stateCode: String, $debug: Boolean!) {\n  getCarModel(\n    pno34: $pno34\n    market: $market\n    modelYear: $modelYear\n    dealerCode: $dealerCode\n    stateCode: $stateCode\n    debug: $debug\n  ) {\n    carModelDetail {\n      pno34\n      modelYear\n      structureWeek\n      description\n      earliestDeliveryDate\n      visualizations {\n        name\n        views {\n          name\n          parameters {\n            urlTemplate\n            parameters {\n              required\n              values\n            }\n          }\n        }\n      }\n      cashPriceData {\n        totals {\n          car {\n            carTotalPrice {\n              id\n              label\n              value\n            }\n            carTotalBasicPrice {\n              id\n              label\n              value\n            }\n            carTotalTaxes {\n              id\n              label\n              value\n            }\n            carTotalVAT {\n              id\n              label\n              value\n            }\n            carTotalPriceExclVAT {\n              id\n              label\n              value\n            }\n            taxes {\n              name\n              amount\n              vatRate\n              vatPosition\n            }\n          }\n          extras {\n            extrasTotalPrice {\n              id\n              label\n              value\n            }\n            extrasBasicPrice {\n              id\n              label\n              value\n            }\n            extrasTotalVAT {\n              id\n              label\n              value\n            }\n            extrasTotalPriceExclVAT {\n              id\n              label\n              value\n            }\n          }\n          delivery {\n            deliveryChargePrice {\n              id\n              label\n              value\n            }\n            deliveryChargeBasicPrice {\n              id\n              label\n              value\n            }\n            deliveryChargeVAT {\n              id\n              label\n              value\n            }\n          }\n          grandTotal {\n            grandTotalCarExtras {\n              grandTotalCarExtrasPrice {\n                id\n                label\n                value\n              }\n              grandTotalCarExtrasBasicPrice {\n                id\n                label\n                value\n              }\n              grandTotalCarExtrasBeforeVAT {\n                id\n                label\n                value\n              }\n              grandTotalCarExtrasTax {\n                id\n                label\n                value\n              }\n              grandTotalCarExtrasVAT {\n                id\n                label\n                value\n              }\n            }\n          }\n        }\n        car {\n          type\n          code\n          price\n          vat\n          priceIncVat\n        }\n        extras {\n          id\n          price\n          vat\n          priceIncVat\n        }\n      }\n      fspPrices {\n        price\n        description\n        financeType\n        priceIsB2B\n      }\n      images {\n        icons {\n          rims\n          color\n          upholstery\n        }\n        location {\n          url\n          angles\n          resolutions\n        }\n        studio {\n          url\n          angles\n          resolutions\n        }\n      }\n      content {\n        excluded\n        filterTypeId\n        featureType\n        code\n        name\n        description\n        numericValue\n        dateValue\n        stringValue\n        images {\n          url\n          alt\n        }\n        thumbnail {\n          url\n          alt\n        }\n        isB2B\n        learnMore {\n          href\n          label\n        }\n        tyreLinks {\n          url\n          label\n        }\n      }\n      towbar {\n        excluded\n        filterTypeId\n        featureType\n        code\n        name\n        description\n        numericValue\n        dateValue\n        stringValue\n        images {\n          url\n          alt\n        }\n        thumbnail {\n          url\n          alt\n        }\n        isB2B\n      }\n      wltpNedcSummary {\n        items {\n          redaName\n          name\n          description\n          value\n          unit\n        }\n      }\n      techData {\n        engineBev_LabelForPower\n        engineBev_TotalHp\n        engineBev_TotalKw\n        engineBev_LabelForTorque\n        engineBev_TotalTorqueNm\n        engineBev_LabelForBattery\n        engineBev_Batteries\n        engineHybrid_Batteries\n        labelForPerformanceRange\n        performance\n        engineBev_ElectricRange\n        engineBev_LabelForElectricMotors\n        engineBev_ElectricMotors\n        engineBev_ElectricRangeEpaMiles\n        labelForDivider\n        productFeatures {\n          excluded\n          featureType\n          code\n          name\n          description\n          images {\n            url\n            alt\n          }\n          thumbnail {\n            url\n            alt\n          }\n          isStandard\n          isDefault\n          isAccessory\n        }\n        engineBev_LabelForTrunkCapacityTotal\n        engineBev_TrunkCapacityTotal\n        seats\n        seatsLabel\n        driveTrain\n        seeAllFeatures {\n          href\n          label\n        }\n      }\n      dimensionsMetric {\n        wheelbaseLabel\n        wheelbaseValue\n        dimensions\n        dimensionsLabel\n      }\n      dimensionsImperial {\n        wheelbaseLabel\n        wheelbaseValue\n      }\n      interior360BaseUrl\n      exterior360BaseUrl\n      exteriorImageCount\n      environmentalImpactDetails {\n        label\n        linkUrl\n        isIncluded\n      }\n      packages {\n        filterTypeId\n        featureType\n        name\n        code\n        cardTitle\n        labelForInfo\n        description\n        numericValue\n        dateValue\n        stringValue\n        isB2B\n        images {\n          url\n          alt\n        }\n        thumbnail {\n          url\n          alt\n        }\n        excluded\n        totalSalesPrice\n        learnMore {\n          href\n          label\n        }\n      }\n    }\n  }\n}\n"}';
-    $car_post_data_array = json_decode($car_postdata, true);
-    //echo $car_post_data_array['variables']['pno34'];
-
-
-    //print_r ($want);
+    // loop thru cars at the dealer.
     foreach ($want as $w) {
-        $car_post_data_array['variables']['pno34'] = $w['pno34'];
-        $car_post_data_array['variables']['dealerCode'] = $r['code'];
-        $car_post_data_array['variables']['modelYear'] = $w['modelYear'];
+        echo $r['name'] . " (" . $r['code'] . "),";
         echo $w['pno34'] . ",";
-
-        $ch = curl_init($url); 
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($car_post_data_array));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        $result = curl_exec($ch);
-        curl_close($ch);
-    
-        $car_array = json_decode($result, true);
-        $want_car = $car_array['data']['getCarModel']['carModelDetail'];
-        
-        echo $want_car['earliestDeliveryDate'] . ",";
+        echo $w['earliestDeliveryDate'] . ",";
         echo $w['cashPriceData']['totals']['car']['carTotalPrice']['value'] . ",";
-/*        echo $want_car['Engine'] . ",";
-        echo $want_car['ModelYear'] . ",";
-*/
-      
-        foreach ($want_car['content'] as $car) {
+        
+        foreach ($w['content'] as $car) {
             if ($car['featureType'] == 'Price')
                 continue;
 
             if ($car['featureType'] == 'DeliveryDate')
                 continue;
-            if ($car['featureType'] == 'Model')
-                continue;
-
-/*            if ($car['featureType'] == 'Engine')
-                continue;
-            if ($car['featureType'] == 'ModelYear')
-                continue;              
-*/
 
             #echo $car['featureType'] . ": ";
             if ($car['featureType'] == 'DeliveryDate') {
-                echo ": " . $car['dateValue'];
+                echo $car['dateValue'];
             } else 
                 echo $car['name'];
             echo ",";
-        }
-        foreach ($want_car['towbar'] as $car) {
-            if ($car['featureType'] == 'Towbar'){
-                echo "Towbar,";
-            }
-            else
-                continue;
         }
     echo "\n";
     }
